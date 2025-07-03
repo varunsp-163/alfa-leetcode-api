d12530960902453bd7995c58a5b47eebcbddb130
Varun S P <spvarun47@gmail.com>
2025-07-03 17:17:36 +0530

    Removed rate limitter

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction, Response } from 'express';
 import cors from 'cors';
-import rateLimit from 'express-rate-limit';
+
 import * as leetcode from './leetCode';
 import { FetchUserDataRequest } from './types';
 import apicache from 'apicache';
@@ -21,17 +21,11 @@ const app = express();
 let cache = apicache.middleware;
 const API_URL = process.env.LEETCODE_API_URL || 'https://leetcode.com/graphql';
 
-const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hour
-  limit: 60,
-  standardHeaders: 'draft-7',
-  legacyHeaders: false,
-  message: 'Too many request from this IP, try again in 1 hour',
-});
+
 
 app.use(cache('5 minutes'));
 app.use(cors()); //enable all CORS request
-app.use(limiter); //limit to all API
+
 app.use((req: express.Request, _res: Response, next: NextFunction) => {
   console.log('Requested URL:', req.originalUrl);
   next();
